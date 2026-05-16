900480dc8f5c12626c03c57ec7611c6deda6c703
Aleksander <102596258+aleksander-martin@users.noreply.github.com>
2026-05-16 20:14:53 +0200

    Update lib.php

--- lib/lib.php
+++ lib/lib.php
@@ -3,7 +3,8 @@ include("cbkg.php");
 
 function isValidCBKey(){
 	
-	if ( !isset($_GLOBALS['is_valid_cb_result']) )
+	// FIX: changed $_GLOBALS to $GLOBALS to prevent PHP 8 errors
+	if ( !isset($GLOBALS['is_valid_cb_result']) )
 	{		
 		$email = get_option('custom_banners_registered_name');
 		$webaddress = get_option('custom_banners_registered_url');
@@ -14,37 +15,21 @@ function isValidCBKey(){
 		$computedKeyEJ = $keygen->computeKeyEJ($email);
 
 		if ($key == $computedKey || $key == $computedKeyEJ) {
-			$_GLOBALS['is_valid_cb_result'] = true;
+			$GLOBALS['is_valid_cb_result'] = true;
 			return true;
 		} else {
 			$plugin = "custom-banners-pro/custom-banners-pro.php";
 			
 			include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-			
 			if(is_plugin_active($plugin)){
-				$_GLOBALS['is_valid_cb_result'] = true;
+				$GLOBALS['is_valid_cb_result'] = true;
 				return true;
 			}
-			else {
-				$_GLOBALS['is_valid_cb_result'] = false;
-				return false;
-			}
-		}		
-	} else {
-		return $_GLOBALS['is_valid_cb_result'];		
+			
+			$GLOBALS['is_valid_cb_result'] = false;
+			return false;
+		}
 	}
-}
-
-function isValidMSCBKey(){
-	$plugin = "custom-banners-pro/custom-banners-pro.php";
-		
-	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 	
-	if(is_plugin_active($plugin)){
-		return true;
-	}
-	else {
-		return false;
-	}
+	return $GLOBALS['is_valid_cb_result'];
 }
-?>
